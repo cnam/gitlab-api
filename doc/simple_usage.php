8767f7cfec62bfd72c7abded9b0d58dcdf958173
cnam <cnam812@gmail.com>
2014-08-20 14:59:21 +0400

    add in guzzle descriptions

--- doc/simple_usage.php
+++ doc/simple_usage.php
@@ -3,6 +3,7 @@
 require __DIR__.'/../vendor/autoload.php';
 
 $request_options = array(
+    "base_url" => "http://gitlab.com/api/v3",
     "request.options" => array(
         "verify" => false,
         'auth' => array('login', 'pass'),
@@ -14,34 +15,8 @@ $request_options = array(
 
 $api = new Gitlab\Api($request_options);
 
-$issues = $api->executeCommand('GetIssues');
-
-foreach ($issues as $issue) {
-    echo($issue->getId().' '.$issue->getTitle()).PHP_EOL;
-}
-
-
 echo 'GetIssuesByProject'.PHP_EOL;
 
-$issues = $api->executeCommand('GetIssuesByProject', array('project_id' => 10));
+$issues = $api->executeCommand('GetIssuesByProject', array('project_id' => 10, "per_page"=>10));
 
-foreach ($issues as $issue) {
-    echo($issue->getId().' '.$issue->getTitle()).PHP_EOL;
-}
-
-echo 'GetIssue'.PHP_EOL;
-
-$issue = $api->executeCommand('GetIssue', array('project_id' => 10,'issue_id'=>58));
-
-echo($issue->getId().' '.$issue->getTitle()).PHP_EOL;
-
-
-$projects = $api->executeCommand('GetProjects');
-
-foreach ($projects as $project) {
-    /**
-     * @var $project \Gitlab\Models\Project
-     */
-    echo($project->getId() . " " . $project->getNameWithNamespace()).PHP_EOL;
-}
-
+echo $issues->count();
