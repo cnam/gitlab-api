bff6f41ba85b343f7847eb7950da3c67d6cb1acb
cnam <cnam812@gmail.com>
2014-08-29 22:09:09 +0400

    add comments for usage

---
+++ src/Gitlab/Models/Comment.php
@@ -0,0 +1,114 @@
+<?php
+namespace Gitlab\Models;
+
+use Guzzle\Service\Command\ResponseClassInterface;
+use Guzzle\Service\Command\OperationCommand;
+
+class Comment implements ResponseClassInterface
+{
+
+    protected $id;
+    protected $body;
+    protected $attachment;
+    /**
+     * @var array
+     */
+    protected $author;
+    protected $created_at;
+
+    public static function fromCommand(OperationCommand $command)
+    {
+        $response = $command->getResponse();
+        $item = $response->json();
+
+        return new self($item);
+    }
+
+    public function __construct(array $item)
+    {
+        foreach ($item as $name => $content) {
+            $this->{$name} = $content;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAttachment()
+    {
+        return $this->attachment;
+    }
+
+    /**
+     * @param mixed $attachment
+     */
+    public function setAttachment($attachment)
+    {
+        $this->attachment = $attachment;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param array $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+}
