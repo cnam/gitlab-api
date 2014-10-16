9315ea6512e56840cac33770f496ca5308b99517
cnam <cnam812@gmail.com>
2014-10-16 22:26:58 +0400

    add api for work with gitlab api group

---
+++ src/Gitlab/Models/Group.php
@@ -0,0 +1,93 @@
+<?php
+namespace Gitlab\Models;
+
+use Guzzle\Service\Command\ResponseClassInterface;
+use Guzzle\Service\Command\OperationCommand;
+
+class Group implements ResponseClassInterface
+{
+
+    protected $id;
+    protected $name;
+    protected $path;
+    protected $owner_id;
+
+    public static function fromCommand(OperationCommand $command)
+    {
+        $response = $command->getResponse();
+        $item = $response->json();
+
+        return new self($item);
+    }
+
+    public function __construct(array $item)
+    {
+        foreach ($item as $name => $content) {
+            $this->{$name} = $content;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOwnerId()
+    {
+        return $this->owner_id;
+    }
+
+    /**
+     * @param mixed $owner_id
+     */
+    public function setOwnerId($owner_id)
+    {
+        $this->owner_id = $owner_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param mixed $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+}
